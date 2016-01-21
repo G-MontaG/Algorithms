ed96b069ff8c8243dee7936262fb4d03158118d9
Arthur Osipenko <arthur.osipenko@gmail.com>
2016-01-22 00:10:17 +0200

    full test for selection sort

--- test/selection-sort.js
+++ test/selection-sort.js
@@ -14,13 +14,37 @@ function isSorted(array) {
   return true;
 }
 
-describe('Selection sort', () => {
+let smallArray = _.map(new Array(100), () => _.random(9));
+let mediumArray = _.map(new Array(1000), () => _.random(9));
+let largeArray = _.map(new Array(10000), () => _.random(9));
 
-  let array = [2,7,3,9,1,6,4,5,6,9,2,6,9,0,2];
+describe('Sorting algorithms', () => {
 
-  it('should sorted array', () => {
-    selection.sort(array);
-    assert.isTrue(isSorted(array));
+  let smallArrayTest;
+  let mediumArrayTest;
+  let largeArrayTest;
+
+  beforeEach(() => {
+    smallArrayTest = _.clone(smallArray);
+    mediumArrayTest = _.clone(mediumArray);
+    largeArrayTest = _.clone(largeArray);
   });
 
+  describe('Selection sort', () => {
+
+    it('sorting small array', () => {
+      selection.sort(smallArrayTest);
+      assert.isTrue(isSorted(smallArrayTest));
+    });
+
+    it('sorting medium array', () => {
+      selection.sort(mediumArrayTest);
+      assert.isTrue(isSorted(mediumArrayTest));
+    });
+
+    it('sorting large array', () => {
+      selection.sort(largeArrayTest);
+      assert.isTrue(isSorted(largeArrayTest));
+    });
+  });
 });
