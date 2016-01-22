1221bb494eeb31a298c5bb389bdd5c282b53421f
Arthur Osipenko <arthur.osipenko@gmail.com>
2016-01-22 14:41:43 +0200

    add insertion sort

---
+++ src/insertion-sort.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const _ = require('lodash');
+
+module.exports = (function() {
+
+  function sort(array) {
+    let size = array.length;
+    for(let i = 1; i < size; i++) {
+      for(let j = i; j > 0 && less(array, j, j-1); j--) {
+        exch(array, i, j);
+      }
+    }
+  }
+
+  function less(array, i, min) {
+    return array[i] < array[min];
+  }
+
+  function exch(array, i, min) {
+    let temp = array[i];
+    array[i] = array[min];
+    array[min] = temp;
+  }
+
+  return {
+    sort: sort
+  };
+
+})();
--- test/sorting.js
+++ test/sorting.js
@@ -4,6 +4,7 @@ const _ = require('lodash');
 const assert = require('chai').assert;
 
 const selection = require('../src/selection-sort');
+const insertion = require('../src/insertion-sort');
 
 function isSorted(array) {
   for(let i = 1, size = array.length; i < size; i++) {
@@ -17,17 +18,20 @@ function isSorted(array) {
 let smallArray = _.map(new Array(100), () => _.random(9));
 let mediumArray = _.map(new Array(1000), () => _.random(9));
 let largeArray = _.map(new Array(10000), () => _.random(9));
+let extraLargeArray = _.map(new Array(50000), () => _.random(9));
 
 describe('Sorting algorithms', () => {
 
   let smallArrayTest;
   let mediumArrayTest;
   let largeArrayTest;
+  let extraLargeArrayTest;
 
   beforeEach(() => {
     smallArrayTest = _.clone(smallArray);
     mediumArrayTest = _.clone(mediumArray);
     largeArrayTest = _.clone(largeArray);
+    extraLargeArrayTest = _.clone(extraLargeArray);
   });
 
   describe('Selection sort', () => {
@@ -46,5 +50,36 @@ describe('Sorting algorithms', () => {
       selection.sort(largeArrayTest);
       assert.isTrue(isSorted(largeArrayTest));
     });
+
+    it('sorting extra large array', () => {
+      selection.sort(extraLargeArrayTest);
+      assert.isTrue(isSorted(extraLargeArrayTest));
+    });
+
   });
+
+  describe('Insertion sort', () => {
+
+    it('sorting small array', () => {
+      insertion.sort(smallArrayTest);
+      assert.isTrue(isSorted(smallArrayTest));
+    });
+
+    it('sorting medium array', () => {
+      insertion.sort(mediumArrayTest);
+      assert.isTrue(isSorted(mediumArrayTest));
+    });
+
+    it('sorting large array', () => {
+      insertion.sort(largeArrayTest);
+      assert.isTrue(isSorted(largeArrayTest));
+    });
+
+    it('sorting extra large array', () => {
+      insertion.sort(extraLargeArrayTest);
+      assert.isTrue(isSorted(extraLargeArrayTest));
+    });
+
+  });
+
 });
