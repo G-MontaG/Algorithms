67e00d5203ab4ba2c5750191f58b2bc7306f833a
Arthur Osipenko <arthur.osipenko@gmail.com>
2016-01-27 19:16:27 +0200

    add vector

---
+++ src/fundamentals/vector.js
@@ -0,0 +1,13 @@
+"use strict";
+
+class Vector {
+  constructor(dimension, vector) {
+    this._dimension = dimension || vector.length || 0;
+    this._vector = vector || [];
+  }
+
+
+
+}
+
+module.exports = Vector;
