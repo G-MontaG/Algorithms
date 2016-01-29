6171c770d416ed6026915b6c7a19aa8954d079fa
Arthur Osipenko <arthur.osipenko@gmail.com>
2016-01-29 19:02:33 +0200

    add sequential search

---
+++ src/searching/sequential-search.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const _ = require('lodash');
+
+class SequentialSearch {
+
+  constructor(items) {
+    this._counter = items.length || 0;
+    this._items = items || [];
+  }
+
+
+
+}
+
+module.exports = SequentialSearch;
