521e73fa1c46c80321d4c6b60d6ffcbb06a7a3e3
Arthur Osipenko <arthur.osipenko@gmail.com>
2016-01-31 18:16:15 +0200

    fix heap sort

--- src/sorting/heap-sort.js
+++ src/sorting/heap-sort.js
@@ -5,8 +5,9 @@ module.exports = (function () {
 
   function sort(array) {
     let size = array.length;
-    for (let k = Math.floor(size/2); k >= 1; k--)
-    _sink(array, k, size);
+    for (let k = size / 2; k >= 1; k--) {
+      _sink(array, k, size);
+    }
     while (size > 1) {
       _exch(array, 1, size--);
       _sink(array, 1, size);
@@ -14,9 +15,9 @@ module.exports = (function () {
   }
 
   function _sink(array, k, size) {
-    while (2*k <= size) {
-      let j = 2*k;
-      if (j < size && _less(array, j, j+1)) {
+    while (2 * k <= size) {
+      let j = 2 * k;
+      if (j < size && _less(array, j, j + 1)) {
         j++;
       }
       if (!_less(array, k, j)) {
@@ -27,14 +28,14 @@ module.exports = (function () {
     }
   }
 
-  function _less(array, i, min) {
-    return array[i] < array[min];
+  function _less(array, i, j) {
+    return array[i - 1] < array[j - 1];
   }
 
-  function _exch(array, i, min) {
-    let temp = array[i];
-    array[i] = array[min];
-    array[min] = temp;
+  function _exch(array, i, j) {
+    let temp = array[i - 1];
+    array[i - 1] = array[j - 1];
+    array[j - 1] = temp;
   }
 
   return {
